2385aa51d61ca978a20f565e91d6afc2746cf351
orlando.leso@gmail.com <orlando.leso@gmail.com>
2022-05-08 23:57:39 -0500

    se inicio el proyecto de lista de tareas por ahcer 'ToDoMachine' se agregaron los archivos necesarios y los componentes asi como sus archivos de estilos

---
+++ src/TodoCounter.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./TodoCounter.css"
+
+function TodoCounter() {
+    return (
+
+        <h2 className="todoCounter" >Has completado 2 de 4 ToDos</h2>
+    );
+}
+
+export { TodoCounter };
